f3527eb5c0ffffa19f9b9a27a4d6f400c5256866
Helder Oliveira <65983803+lireupt@users.noreply.github.com>
2021-11-02 14:49:11 +0000

    Update apontaentos.txt

--- Examples/dom_Examples/Examples/dom_Examples/example_01.js
+++ Examples/dom_Examples/Examples/dom_Examples/example_01.js
@@ -1,1 +1,10 @@
 alert("Olá Mundo!");
+
+window.onload = function(){
+    let result = document.getElementById("results")
+    let button = document.getElementById("myButtons")
+    button.onclick=function(){
+        result.innerHTML = new Date();
+    }
+}
+
